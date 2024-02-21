7d5c4eafea964b67cce9f7485747a1f42d8b7491
Alan Lopez <alanloopez@ciencias.unam.mx>
2024-02-21 12:34:46 -0600

    Final details

--- README.md
+++ README.md
@@ -1,10 +1,49 @@
-# Ingeniería de Software 2024-2
-Repositorio oficial de la materia de Ingenieria de Software de la Facultad de Ciencias de la UNAM del semestre 2024-2
+# P1. Introducción a Python
 
-## Profesores:
+## Entorno Virtual
 
-- Francisco Valdes Souto
-- Valeria Garcia Landa
-- Fernando Fong
-- Erick Martínez Piza
-- Rogelio Alcantar Arenas
+En Windows, ejecuta:
+
+```
+super-env\Scripts\activate
+```
+
+- Ojo: por default windows no deja correr scripts, así está en default:
+
+```
+ Set-ExecutionPolicy Restricted
+```
+
+Es necesario correr en Powershell en Administrador el siguiente comando:
+
+```
+Set-ExecutionPolicy RemoteSigned
+```
+
+En Unix o MacOS, ejecuta:
+
+```
+source super-env/bin/activate
+```
+
+(Este script está escrito para la consola bash. Si usas las consolas csh or fish, hay scripts alternativos activate.csh y activate.fish que deberá usar en su lugar.)
+
+Para desactivar el entorno virtual, digita:
+
+```
+deactivate
+```
+
+## Correr el programa
+
+Primero se debe activar el entorno.
+
+Segundo, se debe activar la base de datos en mysql workbench con puerto localhost:3306.
+
+Después:
+
+```
+python app.py
+```
+
+Cada vez que se ejecuta se agregará denuevo un usuario, pelicula y renta. Para evitar esto, se debe comentar la linea 19 (funcion agregar_tres_entradas).
--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask
-from sqlalchemy import and_, or_
 
 from alchemyClasses import db
-from cryptoUtils.CryptoUtils import cipher
-from hashlib import sha256
+
+from menu import menu
+from utils import agregar_tres_entradas
 
 
-# mysql+pymysql://ferfong:Developer123!@localhost:3306/ing_soft
-# <dialecto>+<driver>://<usuario>:<passwd>@localhost:3306/<db>
-# mysql+pymysql://lab:Developer123!@localhost:3306/lab_ing_soft
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ferfong:Developer123!@localhost:3306/lab_ing_software'
 app.config.from_mapping(
@@ -16,26 +13,8 @@ app.config.from_mapping(
 )
 db.init_app(app)
 
-
 if __name__ == '__main__':
     with app.app_context():
-        # for alumno in Alumno.query.all():  # Select * from alumno
-        #     print(alumno)
-        pass
-        # Create
-        # valeria = Alumno('Valeria', 'Ramirez', 319311918, apMat=None,
-        #                  password=sha256(cipher("Developer123!")).hexdigest())
-        # db.session.add(valeria)
-        # db.session.commit()
-        # Update
-        # Primero tenemos que buscar el objeto que queremos.
-        # Ya que lo tengo, entonces cambio el atributo.
-        # Y entonces hago el commit.
-        # fer = Alumno.query.filter(Alumno.nombre == 'Fernando').first()
-        # print(type(fer))
-        # fer.nombre = "Fer"
-        # fer.ap_mat = "Baeza"
-        # db.session.commit()
-        # Delete
-        # borra_alumno(313320679)
-        # print("Borrado con éxito!")
+
+        agregar_tres_entradas()
+        menu()
